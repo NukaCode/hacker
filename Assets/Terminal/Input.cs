5b828307d7beefd6be538fe57b8bdac060b54c12
RiDdLeS <riddles8888@gmail.com>
2016-11-13 16:18:31 -0600

    Created name space for terminal

---
+++ Assets/Terminal/Input.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+namespace Terminal
+{
+    public class Input : MonoBehaviour
+    {
+
+        InputField input;
+        InputField.SubmitEvent submitEvent;
+        public Text output;
+
+        // Use this for initialization
+        void Start()
+        {
+            input = gameObject.GetComponent<InputField>();
+            submitEvent = new InputField.SubmitEvent();
+            submitEvent.AddListener(SubmitInput);
+            input.onEndEdit = submitEvent;
+        }
+
+        private void SubmitInput(string arg0)
+        {
+            string history = output.text; // Might need ToString()
+            string newText = history + "\n" + arg0;
+            output.text = newText;
+            input.text = "";
+            input.ActivateInputField();
+        }
+    }
+};
